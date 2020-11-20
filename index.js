a27c3942e12a52ebf65df7a9b5c88506274c42bd
milmalho <milmalho@iu.edu>
2020-11-19 20:22:43 -0500

    added async function

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ bot.on('voiceStateUpdate', (oldState, newState) => {
         if(newState.id === "260158083051814923" && newState.channelID === "755094298017398828"){
             newState.member.voice.channel.join()
                 .then(connection => {
-                    setTimeout(() => {
-                        const audio = connection.play("./obi-wan.mp3");
+                    setTimeout(async () => {
+                        const audio = await connection.play("./obi-wan.mp3");
                         audio.on("finish", end => {
                             newState.member.voice.channel.leave();
                         })   
-                    }, 1000);
+                    }, 500);
                     
                 })
                 .catch(err => console.error);
